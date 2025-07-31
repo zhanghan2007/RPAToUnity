35a0677c455d620598e426f455435327b28208f1
zhanghan2007 <150654863+zhanghan2007@users.noreply.github.com>
2025-07-31 14:54:08 +0800

    Update HttpBridgeMethods.cs

--- RPA/Editor/HttpBridgeMethods.cs
+++ RPA/Editor/HttpBridgeMethods.cs
@@ -7,23 +7,23 @@ public class HttpBridgeMethods
     {
         switch (methodName)
         {
-            case "ExecutePSDConversion":
-                var s = ExecutePSDConversion(args);
-                return (200, s ? "转换psd到预制体成功" : "转换psd到预制体失败");
-                break;
+            // case "ExecutePSDConversion":
+            //     var s = ExecutePSDConversion(args);
+            //     return (200, s ? "转换psd到预制体成功" : "转换psd到预制体失败");
+            //     break;
         }
         return (404, "Method not found");
     }
     
     public static bool ExecutePSDConversion(string args)
     {
-        Debug.Log("进入方法");
-        if (!string.IsNullOrEmpty(args))
-        {
-            // 调用目标方法
-            PSDImportMenu.PSDGo(args);
-            return true;
-        }
-        return false;
+        // Debug.Log("进入方法");
+        // if (!string.IsNullOrEmpty(args))
+        // {
+        //     // 调用目标方法
+        //     PSDImportMenu.PSDGo(args);
+        //     return true;
+        // }
+        // return false;
     }
 }
